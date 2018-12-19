db642f9b5f5dfaf69ad1f163d9b2574c1ff0e520
sarathchandrat <sarathchandra51@yahoo.in>
2018-12-19 18:23:41 +0530

    slider widget is added and its functionality in pending and its position is determined

--- display.py
+++ display.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt4 import QtGui
+from PyQt4 import Qtcore
 window_properties={'window_height':500,'window_width':200,'window_Title':'Command Control'}
 def x():
     print('button pressed')
@@ -9,12 +10,37 @@ def window():
     w = QtGui.QWidget()
     w.setGeometry(0,0,window_properties['window_height'],window_properties['window_width'])
     w.setWindowTitle(window_properties['window_Title'])
+
     motor_off_on=QtGui.QPushButton('on',w)
     motor_off_on.setCheckable(True)
     motor_off_on.move(400,10)
+
     motor_clockwise=QtGui.QPushButton('clockwise',w)
     motor_clockwise.setCheckable(True)
-    motor_clockwise.move
+    motor_clockwise.move(100,20)
+
+    motor_anti_clockwise=QtGui.QPushButton('anti clockwise',w)
+    motor_anti_clockwise.setCheckable(True)
+    motor_anti_clockwise.move(100,100)
+
+    left_turn=QtGui.QPushButton('left',w)
+    left_turn.setCheckable(True)
+    left_turn.move(15,60)
+
+    stop=QtGui.QPushButton('stop',w)
+    stop.setCheckable(True)
+    stop.move(100,60)
+
+    right_turn=QtGui.QPushButton('right',w)
+    right_turn.setCheckable(True)
+    right_turn.move(185,60)
+
+    motor_magnitude=QtGui.QSlider(Qt.vertical,w)
+    motor_magnitude.setMinimum(0)
+    motor_magnitude.setMaximum(10)
+
+
+
     #c.toggle()
     #c.setText('start')
     #b.move(10,10)
