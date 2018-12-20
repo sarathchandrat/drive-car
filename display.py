f28aea4412b2ceb7a3a8c59f2913d6b2320099cc
sarathchandrat <sarathchandra51@yahoo.in>
2018-12-20 18:26:29 +0530

    all the buttons and sliders are fixed but their actions and their fuctioning scenario is pending

--- display.py
+++ display.py
@@ -7,44 +7,63 @@ def x():
 def window():
     global window_properties
     app = QtGui.QApplication(sys.argv)
+    #layout=QtGui.QGridLayout()
     w = QtGui.QWidget()
     w.setGeometry(0,0,window_properties['window_height'],window_properties['window_width'])
     w.setWindowTitle(window_properties['window_Title'])
 
     motor_off_on=QtGui.QPushButton('on',w)
     motor_off_on.setCheckable(True)
-    motor_off_on.move(400,10)
+    motor_off_on.setFixedSize(50,25)
+    motor_off_on.move(450,10)
+
+    #layout.addWidget(motor_off_on,0,40)
 
     motor_clockwise=QtGui.QPushButton('clockwise',w)
-    motor_clockwise.setCheckable(True)
-    motor_clockwise.move(100,20)
+    motor_clockwise.setCheckable(False)
+    motor_clockwise.setFixedSize(75,25)
+    motor_clockwise.move(90,20)
+
+    #layout.addWidget(motor_clockwise,15,20)
 
     motor_anti_clockwise=QtGui.QPushButton('anti clockwise',w)
-    motor_anti_clockwise.setCheckable(True)
-    motor_anti_clockwise.move(100,100)
+    motor_anti_clockwise.setCheckable(False)
+    motor_anti_clockwise.setFixedSize(100,25)
+    motor_anti_clockwise.move(80,100)
+    #layout.addWidget(motor_anti_clockwise)
 
     left_turn=QtGui.QPushButton('left',w)
-    left_turn.setCheckable(True)
-    left_turn.move(15,60)
+    left_turn.setCheckable(False)
+    left_turn.setFixedSize(50,25)
+    left_turn.move(35,60)
+    #layout.addWidget(left_turn)
 
     stop=QtGui.QPushButton('stop',w)
-    stop.setCheckable(True)
+    stop.setCheckable(False)
+    stop.setFixedSize(40,25)
     stop.move(100,60)
+    #layout.addWidget(stop)
 
     right_turn=QtGui.QPushButton('right',w)
-    right_turn.setCheckable(True)
-    right_turn.move(185,60)
-    '''
-    motor_magnitude=QtGui.QSlider(Qt.vertical,w)
+    right_turn.setCheckable(False)
+    right_turn.move(150,60)
+    right_turn.setFixedSize(50,25)
+    #layout.addWidget(right_turn)
+    motor_magnitude=QtGui.QSlider(QtCore.Qt.Horizontal,w)
     motor_magnitude.setMinimum(0)
     motor_magnitude.setMaximum(10)
-    '''
-
+    motor_magnitude.setValue(0)
+    motor_magnitude.setTickPosition(QtGui.QSlider.TicksBelow)
+    motor_magnitude.setTickInterval(1)
+    motor_magnitude.move(380,60)
+    #layout.addWidget(motor_magnitude)
+    #w.setLayout(layout)
 
     #c.toggle()
     #c.setText('start')
     #b.move(10,10)
     w.show()
+    print(motor_off_on.clicked[bool])
     motor_off_on.clicked[bool].connect(x)
     #c.toggle()
     sys.exit(app.exec_())
---
+++ main.py
@@ -0,0 +1,3 @@
+from PyQt4 import QtGui
+from PyQt4 import QtCore
+ def
